1298ed14fcbf589108ef6eb6b805cb0917821e06
bryonkucharski <bryonkucharski@gmail.com>
2018-01-22 19:51:18 -0500

    Added gridworld and rl agent

---
+++ python scripts/gridworld/gridworld.py
@@ -0,0 +1,202 @@
+'''
+
+    Bryon Kucharski
+    Wentorth Institute of Technology
+    Spring 2018
+    
+    My implementation of a gridworld enviornment
+'''
+import numpy as np
+import os, sys, inspect
+import time
+
+#this is just to import stddraw from a different folder
+princeton_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../princeton_lib")))
+if princeton_subfolder not in sys.path:
+    sys.path.insert(0, princeton_subfolder)
+
+import stddraw
+
+#this is just to import rl agent from a different folder
+rl_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../rl")))
+if rl_subfolder not in sys.path:
+    sys.path.insert(0, rl_subfolder)
+
+from qlearning_agent import rl_agent
+
+
+GRID_NUM = 4
+GRID_SIZE = 100 # in pixels
+WIDTH = GRID_SIZE*GRID_NUM
+HEIGHT = WIDTH
+UPDATE_FREQ = 20#HZ
+
+#RL constants
+EPSILON = 0.9   # greedy police
+ALPHA = 0.5     # learning rate
+GAMMA = 1       # discount factor
+
+def drawScene(black_hole_x, black_hole_y, ball_x, ball_y, goal_x, goal_y, neg_x, neg_y):
+    '''
+    handles the drawing of the ball, black box, goal box, and negative box
+    '''
+    stddraw.clear()
+    stddraw.setPenColor(stddraw.BLACK)
+
+    #gridlines
+    for i in range(1,GRID_NUM):
+        stddraw.filledRectangle(GRID_SIZE * i,0,0,HEIGHT)
+        stddraw.filledRectangle(0,GRID_SIZE*i,WIDTH,0)
+    
+    #black rectangle
+    stddraw.filledRectangle(black_hole_x * GRID_SIZE, black_hole_y * GRID_SIZE, GRID_SIZE,GRID_SIZE)
+    
+    #Ball
+    stddraw.setPenColor(stddraw.GREEN)
+    stddraw.filledCircle(((ball_x *GRID_SIZE) + (GRID_SIZE/2)) ,((ball_y *GRID_SIZE) + (GRID_SIZE/2)), GRID_SIZE/2)
+
+    #Goal
+    stddraw.setPenColor(stddraw.GREEN)
+    stddraw.rectangle(goal_x * GRID_SIZE,goal_y * GRID_SIZE,GRID_SIZE, GRID_SIZE)
+
+    #Negative box
+    stddraw.setPenColor(stddraw.RED)
+    stddraw.rectangle(neg_x * GRID_SIZE,neg_y * GRID_SIZE,GRID_SIZE, GRID_SIZE)
+
+def check_valid_move(ball_x,ball_y, action):
+    '''
+    checks if the action about to be taken will result in the ball being off the screen or inside the black hole
+    '''
+    if action == 0: #wants to move up
+        #check if next move will be inside black hole
+        if (ball_y + 1 == black_hole_loc_y) and (ball_x == black_hole_loc_x):
+            return False
+        else:
+            return (ball_y < (GRID_NUM-1))
+
+    elif action == 1: # wants to move 
+        #check if next move will be inside black hole
+        if (ball_y - 1 == black_hole_loc_y) and (ball_x == black_hole_loc_x):
+            return False
+        else:
+            return (ball_y > 0)
+        
+    elif action == 2: #wants to move right
+        #check if next move will be inside black hole
+        if (ball_x + 1 == black_hole_loc_x) and (ball_y == black_hole_loc_y):
+            return False
+        else: 
+            return ball_x < (GRID_NUM - 1)
+
+    elif action == 3:
+        #check if next move will be inside black hole
+        if (ball_x - 1 == black_hole_loc_x) and (ball_y == black_hole_loc_y):
+            return False
+        else:
+            return ball_x > 0
+   
+
+
+stddraw.setXscale(0, WIDTH)
+stddraw.setYscale(0, HEIGHT)
+stddraw.setCanvasSize(WIDTH,HEIGHT)
+
+#initalize the black box, ball, goal location, and negative location
+black_hole_loc_x = np.random.randint(1,GRID_NUM - 1)
+black_hole_loc_y = np.random.randint(1,GRID_NUM - 1)
+ball_x = 0
+ball_y = 0
+
+#goal will always be upper right
+goal_x = GRID_NUM - 1
+goal_y = GRID_NUM - 1
+
+#this is just so the red box is never in the same location as the black box
+while True:
+    neg_x = np.random.randint(1,GRID_NUM - 1)
+    neg_y = np.random.randint(1,GRID_NUM - 1)
+    if ((neg_x is not black_hole_loc_y) and (neg_y is not black_hole_loc_y)):
+        break
+
+
+
+agent = rl_agent(
+                list(range(4)), #UP,DOWN,LEFT,RIGHT
+                GAMMA, #gamma
+                ALPHA,
+                EPSILON
+                )
+
+agent.init_q_table()
+
+drawScene(black_hole_loc_x, black_hole_loc_y,ball_x,ball_y,goal_x,goal_y, neg_x,neg_y)
+
+start = time.time()
+itr = 0
+steps = 0
+terminate  = False
+
+while True:
+    
+    end = time.time()
+    elapsed = end - start
+
+    if elapsed > 1/UPDATE_FREQ: #how fast the ball should move
+        start = time.time()
+
+        #get current state
+        state = [ball_x, ball_y]
+
+        #get action
+        action = agent.get_action(str(state))
+
+        #check if enviornment will allow the new state
+        is_valid = check_valid_move(ball_x, ball_y, action)
+
+        print('ACTON: ' + str(action))
+        print('Is this a valid Move: ' + str(check_valid_move(ball_x, ball_y, action)))
+
+        if is_valid:
+            if action == 0: #UP
+                ball_y = ball_y + 1
+            elif action == 1: #DOWN
+                ball_y = ball_y - 1
+            elif action == 2: #RIGHT
+                ball_x = ball_x + 1
+            elif action == 3: #LEFT
+                ball_x = ball_x - 1
+
+ 
+        #reward decision  -.04 every action, 1 for making it to the goal, -1 for entering red box
+        reward = -.04
+        if((ball_x == goal_x) and (ball_y == goal_y)):
+            reward = 1
+            terminate = True
+        elif((ball_x == neg_x) and (ball_y == neg_y)):
+            reward = -1
+        
+        #get next state
+        state_prime = [ball_x, ball_y]
+
+        #update based on experience {s,a,r,s'}
+        agent.update(str(state),action,reward,str(state_prime))
+
+        #if ball is in goal reset back to 0,0
+        if terminate:
+            ball_x = 0
+            ball_y = 0
+            itr = itr + 1
+            terminate = False
+            steps = 0
+
+        steps = steps + 1
+
+        print('itr: ' + str(itr) + ' steps: ' + str(steps) + ' state: ' + str(state) +' reward: ' + str(reward))
+
+        agent.print_q_table()
+
+
+    drawScene(black_hole_loc_x, black_hole_loc_y,ball_x,ball_y,goal_x,goal_y, neg_x,neg_y)
+    stddraw.show(1)
+
+
---
+++ python scripts/rl/qlearening_agent_empty.py
@@ -0,0 +1,129 @@
+'''
+
+    Bryon Kucharski
+    Wentorth Institute of Technology
+    Spring 2018
+
+    Empty framwork for rl agent
+    
+'''
+
+import numpy as np
+from collections import defaultdict
+
+class rl_agent:
+    
+    def __init__(self,A,gamma,alpha,epsilon):
+        '''
+        Constructor
+
+        ARGS:
+            A: a list of actions the agent has the possibility to take
+                ex - [0,1,2,3] for up,down,right,left
+            gamma: the discount factor. How much the Q(s',a') is taken into consideration when updating the q table
+            alph: the learning rate. How fast the agent learns
+            epsilon: 'greediness' of the agent. How often the agent should explore vs follow the q table
+        '''
+
+        ##### UPDATE THIS #####
+        self.actions = 
+        self.gamma = 
+        self.alpha = 
+        self.epsilon = 
+        ##### #####
+    
+    def init_q_table(self):
+        '''
+        the q table is a defaultdict. The lambda expression is fancy for saying "Everytime a new row (state) is added to the table, initalize the values as 0
+        *to use the default dict the state must be in a string 
+            ex - 
+           
+                d = defaultdict(lambda: [0.0,0.0,0.0,0.0])
+            
+                state = str([0,1])
+                row = d[state]
+                print(row) #returns [0.0,0.0,0.0,0.0], or whatever is in lambda function
+
+                num = d[state][0]
+                print(num) # returns 0 
+
+                d[state][0] = 10
+
+                row = d[state]
+                print(row) #returns [10,0.0,0.0,0.0], or whatever is in lambda function
+                
+                num = d[state][0]
+                print(num) # returns 10
+
+        '''
+        ##### UPDATE THIS #####
+        self.q_table = 
+        ##########
+   
+    def get_q_table(self):
+        return self.q_table
+
+    def update(self, s, a, r, s_):
+        '''
+        update the q table at the given state, action point
+
+        ARGS:
+            s: current state of the environment
+            a: action just taken in the environment
+            r: reward from the action just taken in the enviornment
+            s_: the next state, after taking the action
+        '''
+       
+        ##### UPDATE THIS #####
+        exp =  # determine the expected value of the next step
+        current =  # determmine the current value of the current step
+        ##########
+
+        '''
+        #For debugging
+        print('current q: ' + str(current))
+        print('current a : ' + str(a))
+        print('r : ' + str(r))
+        print('s: ' + str(s))
+        print('s_: ' + str(s_))
+        #print('a_: ' + str(a_))
+        print('exp: ' + str(exp))
+        '''
+        ##### UPDATE THIS #####
+        self.q_table[s][a] =   # update the spot in the q table    
+        ##########
+
+    def get_action(self, state):
+        '''
+        go to colum of given state
+        pick action with highest q value
+
+        ARGS:
+            state = list of current state as a string 
+                ex - str([0,0])
+        RETURNS:   
+            self.epsilon % of the time - One of the actions defined in self.actions with the highest Q value 
+            1-self.epsilon % of the time - a random action
+        '''
+        ##### UPDATE THIS #####
+        if np.random.rand() < self.epsilon:
+            action = #argmax of the row given the current state
+        else:
+            action = #choose any action
+        #########
+        return action
+    
+    def print_q_table(self):
+        '''
+        prints all values currently in the q table
+        '''
+        for item in self.q_table.items():
+            print(str(item))
+
+    ''' 
+    def save_q_table(self, name):
+        np.save(name,self.q_table)
+
+    def load_q_table(self, table):
+        self.q_table = table
+    '''
